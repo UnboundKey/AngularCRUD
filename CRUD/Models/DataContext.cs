fe5342c111e3600595c2c38ecedadbd6d69dc75b
Benjamin Nordin <godiset@gmail.com>
2022-09-20 10:39:32 -0700

    Initial Commit

---
+++ CRUD/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace CRUD.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
